fea5f2a4805f89e7106523414c1cd3e7aa2fc0a1
b-wagn <crypto@benedikt-wagner.dev>
2026-07-15 09:37:20 +0200

    Show order of generator and  action

--- EthCryptographySpecs/Proofs/Bls.lean
+++ EthCryptographySpecs/Proofs/Bls.lean
@@ -2,6 +2,7 @@ import EthCryptographySpecs.Proofs.Bls.FpZMod
 import EthCryptographySpecs.Proofs.Bls.Fr
 import EthCryptographySpecs.Proofs.Bls.FrZMod
 import EthCryptographySpecs.Proofs.Bls.G1Group
+import EthCryptographySpecs.Proofs.Bls.G1Order
 
 /-!
 # `EthCryptographySpecs.Proofs.Bls`
